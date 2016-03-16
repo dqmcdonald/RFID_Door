ca6be3a0b1a4492a8ca3431ecdfdca9caa1fc687
Quentin McDonald <Quentin.McDonald@schrodinger.com>
2016-03-16 13:19:19 +1300

    Added Exit button, bi color LED for status

--- RFID_Door.ino
+++ RFID_Door.ino
@@ -25,18 +25,35 @@
 #define SOLENOID_PIN 16
 #define BUZZER_PIN 8
 #define LEARN_BUTTON_PIN 9
+#define EXIT_BUTTON_PIN 10
+#define LED_ORANGE_PIN 6
+#define LED_GREEN_PIN 7
 #define OPEN_DURATION 4000
 
+#define LED_OFF    0
+#define LED_ORANGE 1
+#define LED_GREEN  2
+
 #define MAX_CARDS 10
+
+
 
 byte num_cards = 0;
 byte card_ids[MAX_CARDS*4];
 bool found_card = false;
 
+
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance.
 
 void writeCardIDs();
 void readCardIDs();
+
+// Sets the BiColor LED state
+void setLED( int led_state );
+
+void openDoor();
+
 
 void setup() {
   Serial.begin(9600);	// Initialize serial communications with the PC
@@ -49,11 +66,25 @@ void setup() {
 
   pinMode( LEARN_BUTTON_PIN, INPUT );
   digitalWrite( LEARN_BUTTON_PIN, HIGH );
+
+  pinMode( EXIT_BUTTON_PIN, INPUT );
+  digitalWrite( EXIT_BUTTON_PIN, HIGH );
+
+  pinMode( LED_ORANGE_PIN, OUTPUT );
+  pinMode( LED_GREEN_PIN, OUTPUT );
+
   readCardIDs();
 }
 
 void loop() {
-  // LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);  
+  //LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);  
+
+  // Check to see if the exit button is pressed:
+  int exit_val = digitalRead( EXIT_BUTTON_PIN );
+  if( exit_val == 0 ) {
+    openDoor();
+  } 
+
 
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
@@ -105,33 +136,28 @@ void loop() {
       found_card = true;
   }
 
+
+
+
+
   if( found_card ) {
 
+    openDoor();
 
-    digitalWrite( SOLENOID_PIN, HIGH );
-    //    Serial.println(mfrc522.uid.uidByte[0], HEX);
-    //   Serial.println(mfrc522.uid.uidByte[1], HEX);
-    tone( BUZZER_PIN, 220, OPEN_DURATION );
-    delay( OPEN_DURATION );
-    mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-    Serial.println(mfrc522.uid.uidByte[0]);
-
-
-    digitalWrite(SOLENOID_PIN, LOW );
   }
   else {
     // Dump debug info about the card. PICC_HaltA() is automatically called.
     mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
     Serial.println(mfrc522.uid.uidByte[0]);
-
+    setLED( LED_ORANGE );
     tone( BUZZER_PIN, 440, 250 );
     delay(500);
     tone( BUZZER_PIN, 220, 250 );
     delay(500);
     tone( BUZZER_PIN, 440, 250 );
     delay(500);
+    setLED( LED_OFF );
   }
-
 
 }
 
@@ -162,6 +188,53 @@ void readCardIDs( )
   }
 
 }
+
+// Sets the BiColor LED state
+void setLED( int led_state )
+{
+  switch( led_state ) {
+
+  case LED_OFF:
+    digitalWrite( LED_ORANGE_PIN, LOW );
+    digitalWrite( LED_GREEN_PIN, LOW );
+    break;
+
+  case LED_ORANGE:
+    digitalWrite( LED_ORANGE_PIN, HIGH );
+    digitalWrite( LED_GREEN_PIN, LOW );
+    break;
+
+  case LED_GREEN:
+    digitalWrite( LED_ORANGE_PIN, LOW );
+    digitalWrite( LED_GREEN_PIN, HIGH );
+    break;   
+
+
+  default:
+    break;
+  } 
+
+
+}
+
+void openDoor(void ) {
+
+  digitalWrite( SOLENOID_PIN, HIGH );
+  //    Serial.println(mfrc522.uid.uidByte[0], HEX);
+  //   Serial.println(mfrc522.uid.uidByte[1], HEX);
+  setLED( LED_GREEN );
+  tone( BUZZER_PIN, 220, OPEN_DURATION );
+  delay( OPEN_DURATION );
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  Serial.println(mfrc522.uid.uidByte[0]);
+  digitalWrite(SOLENOID_PIN, LOW );
+  setLED( LED_OFF );
+}
+
+
+
+
+
 
 
 
